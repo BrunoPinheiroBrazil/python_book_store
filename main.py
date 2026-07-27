0f2a679dff7dcca59d2ab7c85d8ba6f429386760
Bruno <brunopinheirobrazil@gmail.com>
2026-07-27 11:40:09 -0300

    fix on host and pipeline

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    # O 0.0.0.0 é a chave mágica que abre o Flask para o mundo
+    app.run(host='0.0.0.0', debug=True, port=5002)
